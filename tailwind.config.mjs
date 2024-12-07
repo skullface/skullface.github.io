de15bbc08638cd0ea5988b188ad9c31eb32648ab
skullface <skullface@users.noreply.github.com>
2024-12-07 14:34:25 -0500

    tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,10 +3,16 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
     colors: {
-      primary: 'rgb(var(--color-primary))',
-      foreground: 'rgb(var(--color-foreground))',
+      primary: 'var(--color-primary)',
+      foreground: 'var(--color-foreground)',
       midground: 'var(--color-midground)',
-      background: 'rgb(var(--color-background))',
+      background: 'var(--color-background)',
+    },
+    fontFamily: {
+      sans: ['ARS Maquette', '-apple-system', 'Avenir Next', 'Avenir',
+      'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica', 'Arial', 'sans-serif'],
+      serif: ['Source Serif 4', 'ui-serif', 'Georgia', 'Times New Roman', 'serif'],
+      mono: ['Courier Prime', 'ui-monospace', 'Courier New', 'mono'],
     },
 		extend: {
       maxWidth: {
